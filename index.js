7c384f21e3f0060e080a49d91cd7b9c5410771e6
Jakob Fix <jakob.fix@gmail.com>
2017-02-05 16:16:41 +0100

    :lipstick: indentation issues fixed, better logging, .env file location no longer hard coded

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-require('dotenv').config({path: '/home/pi/dash-button/.env'});
+var path = require('path');
+require('dotenv').config({path: path.join(__dirname, '.env')});
 require('colors');
-
 var client = require('twilio')(process.env.TWILIO_ACC_SID, process.env.TWILIO_AUTH_TOKEN);
 var dash_button = require('node-dash-button');
 var greeting = require('greeting');
 var moment = require('moment');
 var request = require('request');
 var storage = require('node-persist');
+
 
 storage.initSync({ttl: 8*60*60*1000});
 
@@ -26,7 +27,7 @@ dash.on("detected", function (dash_id){
     // OFFICE ARRIVAL/DEPARTURE
 
     if (dash_id === process.env.MAC_SMS) {
-        console.log("Button pressed!".yellow);
+        console.log("🛎  Button pressed!".yellow);
 
         var smsMessage = '';
 
@@ -35,6 +36,7 @@ dash.on("detected", function (dash_id){
                 smsMessage = process.env.SMS_MSG_MORNING;
                 storage.setItemSync("morning_sms", true);
             } else {
+                console.log('✗ Not sending SMS as it already has been sent.'.yellow);
                 return;    
             }
         } else if (isEvening()) {
@@ -42,17 +44,18 @@ dash.on("detected", function (dash_id){
                 smsMessage = process.env.SMS_MSG_EVENING;
                 storage.setItemSync("evening_sms", true);
             } else {
+                console.log('✗ Not sending SMS as it already has been sent.'.yellow);
                 return;
             }
         } else {
-	    smsMessage = process.env.SMS_MSG_UNSPECIFIED;
+            smsMessage = process.env.SMS_MSG_UNSPECIFIED;
         }
         smsMessage = `${greeting.random()}, ${smsMessage}`;
         console.log(`We'll be sending this message: ${smsMessage}`);
 
         // TWILIO APPROACH
-	client.messages.create({
-	    to: process.env.TWILIO_TO_NUMBER,
+        client.messages.create({
+            to: process.env.TWILIO_TO_NUMBER,
             from: process.env.TWILIO_FROM_NUMBER,
             body: smsMessage
         }, function(err, msg) {
@@ -66,9 +69,9 @@ dash.on("detected", function (dash_id){
     // COFFEE CONSUMED
 
     } else if (dash_id === process.env.MAC_COFFEE) {
-        console.log("Somebody just poured a coffee.");
         var date = moment().format('YYYY-MM-DD');
         var time = moment().format('HH:mm'); 
+        console.log(`${date} ${time}: Somebody just poured a coffee.`);
 
         // IFTTT APPROACH
         request({
@@ -79,7 +82,7 @@ dash.on("detected", function (dash_id){
            if (err) {
                console.log('ERRROR: ' + JSON.stringify(err));
            }
-	   console.log(body);
+       console.log(body);
         });
 
     // NEW, UNKNOWN BUTTON FOUND
@@ -88,4 +91,4 @@ dash.on("detected", function (dash_id){
         console.log("Found a new Button: " + dash_id);
     }
 });
-console.log("Listening for button presses ...".green)
+console.log("👂  Listening for button presses ...".green)
